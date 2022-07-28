921fc1df85f8315caea8ba4edfebb769f96c24fa
Seeky <58006653+SeekyCt@users.noreply.github.com>
2022-07-28 14:31:16 +0100

    Basic FORCEFILES generation

---
+++ forcefilesgen.py
@@ -0,0 +1,20 @@
+"""
+Add forcefiles entries to an LCF file
+"""
+
+from argparse import ArgumentParser
+
+hex_int = lambda s: int(s, 16)
+parser = ArgumentParser(description="Add forceactive + forcefiles entries to an LCF file")
+parser.add_argument("lcf_path", type=str, help="LCF input path")
+parser.add_argument("out_path", type=str, help="LCF output path")
+parser.add_argument("forcefiles", type=str, nargs='*', help="LCF output path")
+args = parser.parse_args()
+
+with open(args.lcf_path) as f:
+    txt = f.read()
+
+txt = txt.replace("PPCDIS_FORCEFILES", '\n'.join(args.forcefiles))
+
+with open(args.out_path, 'w', encoding="shift-jis") as f:
+    f.write(txt)
