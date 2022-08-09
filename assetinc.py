088c8a9aca0cb9cc03ed1891db2c26d0ca96d4f2
Seeky <58006653+SeekyCt@users.noreply.github.com>
2022-08-09 15:08:30 +0100

    Remove duplicate format_bytes

--- assetinc.py
+++ assetinc.py
@@ -4,13 +4,7 @@ Generate an array include from an asset
 
 from argparse import ArgumentParser
 
-def format_bytes(dat: bytes, width: int) -> str:
-    return ', \n'.join(
-        ', '.join(
-            f"0x{x:02x}" for x in dat[i:i+width]
-        )
-        for i in range(0, len(dat), width)
-    )
+from ppcdis import format_bytes
 
 if __name__ == "__main__":
     hex_int = lambda s: int(s, 16)
