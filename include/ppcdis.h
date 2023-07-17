6493112072e2fefb92baa833b75c9d378fa81925
Seeky <58006653+SeekyCt@users.noreply.github.com>
2023-07-17 18:05:03 +0100

    Add extra save/rest gpr/fpr decls

--- include/ppcdis.h
+++ include/ppcdis.h
@@ -99,6 +99,10 @@ void _savegpr_24(void);
 void _savegpr_25(void);
 void _savegpr_26(void);
 void _savegpr_27(void);
+void _savegpr_28(void);
+void _savegpr_29(void);
+void _savegpr_30(void);
+void _savegpr_31(void);
 
 void _restgpr_14(void);
 void _restgpr_15(void);
@@ -114,6 +118,10 @@ void _restgpr_24(void);
 void _restgpr_25(void);
 void _restgpr_26(void);
 void _restgpr_27(void);
+void _restgpr_28(void);
+void _restgpr_29(void);
+void _restgpr_30(void);
+void _restgpr_31(void);
 
 void _savefpr_14(void);
 void _savefpr_15(void);
@@ -129,6 +137,10 @@ void _savefpr_24(void);
 void _savefpr_25(void);
 void _savefpr_26(void);
 void _savefpr_27(void);
+void _savefpr_28(void);
+void _savefpr_29(void);
+void _savefpr_30(void);
+void _savefpr_31(void);
 
 void _restfpr_14(void);
 void _restfpr_15(void);
@@ -144,6 +156,10 @@ void _restfpr_24(void);
 void _restfpr_25(void);
 void _restfpr_26(void);
 void _restfpr_27(void);
+void _restfpr_28(void);
+void _restfpr_29(void);
+void _restfpr_30(void);
+void _restfpr_31(void);
 
 void __div2u(void);
 void __div2i(void);
